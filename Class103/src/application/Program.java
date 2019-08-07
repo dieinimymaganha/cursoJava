3f12cc543086779cee9050ec273787bbef251317
Dieinimy Maganha <dieinimy@gmail.com>
2019-08-07 13:36:49 -0300

    Utilizando polimorfismo

--- Class103/src/application/Program.java
+++ Class103/src/application/Program.java
@@ -1,27 +1,19 @@
 package application;
 
 import entities.Account;
-import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		// withdraw da classe Account deve descontar ficando 795.0
-		Account acc1 = new Account(1001, "Alex", 1000.0);
-		acc1.withdraw(200.0);
-		System.out.println(acc1.getBalance());
-
-		// withdraw da class SavingsAccount não deve descontar ficando 800.00
-		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
-		acc2.withdraw(200.0);
-		System.out.println(acc2.getBalance());
+		Account x = new Account(1020, "Alex",1000.0);
+		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
 		
-		// withdraw da classe BusinessAccount descontara 5.0 da Classe Account + 2 da classe BusinessAccount
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-		acc3.withdraw(200.0);
-		System.out.println(acc3.getBalance());
+		x.withdraw(50.0);
+		y.withdraw(50.0);
+		
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
 		
 	}
 
