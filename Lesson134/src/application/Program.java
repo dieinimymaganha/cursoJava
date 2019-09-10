515693dd38cbca5830a93eecc85d6bdee033b97b
Dieinimy Maganha <dieinimy@gmail.com>
2019-09-09 21:36:30 -0300

    Fazendo alguns testes com as tratativas realizadas anteriores e verificando o stack trace

--- Lesson134/src/application/Program.java
+++ Lesson134/src/application/Program.java
@@ -6,23 +6,35 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
+		method1();
+
+		System.out.println("End of program");
+
+	}
+	
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		method2();
+		System.out.println("***METHOD1 END***");
+	}
+
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
 		Scanner sc = new Scanner(System.in);
 		try {
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
-			System.out.println(vect[position]);			
-		}
-		catch (ArrayIndexOutOfBoundsException e){
+			System.out.println(vect[position]);
+		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position!");
-		}
-		catch (InputMismatchException e) {
+			// e.printStackTrace();
+			sc.next();
+		} catch (InputMismatchException e) {
 			System.out.println("Input error!");
 		}
-		System.out.println("End of program");
-		
 		sc.close();
-
+		System.out.println("***METHOD2 END***");
 	}
 
 }
